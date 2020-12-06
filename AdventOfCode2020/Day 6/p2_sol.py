0465c543d13d272aaef79aa99547ec67a0956c8c
dagleaves <68761152+dagleaves@users.noreply.github.com>
2020-12-06 10:10:13 -0500

    Add Day 6

---
+++ AdventOfCode2020/Day 6/p2_sol.py
@@ -0,0 +1,27 @@
+groups = []
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+    index = 0
+    for i in range(len(lines)):
+        line = lines[i]
+        if line == '':
+            index += 1
+        elif i == 0:
+            groups.append(line)
+        elif lines[i-1] == '':
+            groups.append(line)
+        else:
+            groups[index] += f' {line}'
+
+result = 0
+for group in groups:
+    people = group.split()
+
+    intersection = 'abcdefghijklmnopqrstuvwxyz'
+    for person in people:
+        intersection = set.intersection(set(intersection), set(person))
+    result += len(intersection)
+
+print(f'Result: {result}')
